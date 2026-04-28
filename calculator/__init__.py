25b4019f517c4961fe8fe556f162c5c0d4b5d178
amaradakPBI <amar.nath-adak@portfoliobi.com>
2026-04-28 15:49:59 +0530

    Add scientific calculator application

---
+++ calculator/__init__.py
@@ -0,0 +1,4 @@
+from .constants import CONSTANTS, constant_search
+from .conversions import UNIT_CATEGORIES, convert_value, parse_inline_conversion
+from .engine import CalculationResult, CalculatorEngine, display_in_bases
+from .storage import CalculatorStore, HistoryEntry, SettingsState
